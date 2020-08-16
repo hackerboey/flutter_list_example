b205248b2fc2170641e3e318baa95cf7430996db
Manish Kumar <38438569+hackerboey@users.noreply.github.com>
2020-08-16 06:22:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_list_example
+ A new Flutter application demonstrarting llist of data.
---
+++ android/app/src/main/kotlin/com/hackerboey/flutter_list_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hackerboey.flutter_list_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
